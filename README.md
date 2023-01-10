ff4a939a3516796dc77362626959b46cff37c5ee
ophub <ophub2020@gmail.com>
2023-01-10 13:17:06 +0000

    Add output log list option

--- README.md
+++ README.md
@@ -14,7 +14,6 @@ Introduce this Actions in the `.github/workflows/*.yml` workflows script to use,
 - name: Delete releases and workflows runs
   uses: ophub/delete-releases-workflows@main
   with:
-    delete_tags: true
     releases_keep_latest: 5
     workflows_keep_day: 10
     gh_token: ${{ secrets.GH_TOKEN }}
@@ -22,15 +21,16 @@ Introduce this Actions in the `.github/workflows/*.yml` workflows script to use,
 
 ## Parameter description / 参数说明
 
-| parameter/参数           | Defaults/默认值        | Description/说明                                                  |
-|-------------------------|-----------------------|-------------------------------------------------------------------|
-| repo                    | `$GITHUB_REPOSITORY`  | [Optional] Current running repository. <br />[可选项] 当前运行仓库。 |
-| delete_tags             | true                  | [Optional] Delete tags associated with releases. <br />[可选项] 删除与 Releases 关联的 tags。 |
-| releases_keep_latest    | None / 无             | [Required] Number of latest releases reserved. <br />[必选] 最新 Releases 保留数量。 |
-| releases_keep_keyword   | None / 无             | [Optional] The keywords of the releases that need to be reserved. <br />[可选项] 需要保留的 Releases 的关键字。 |
-| workflows_keep_keyword  | None / 无             | [Optional] The keywords of workflows runs that need to be reserved. <br />[可选项] 需要保留的 workflows 运行记录的关键字。 |
-| workflows_keep_day      | 30                    | [Optional] Set the workflows runs retention days. <br />[可选项] 设置 workflows 运行记录保留天数。 |
-| gh_token                | None / 无             | [Required] Set the [GH_TOKEN](https://github.com/ophub/amlogic-s9xxx-armbian/tree/main/build-armbian/documents#2-set-the-privacy-variable-github_token) for performing delete operations. <br />[必选] 设置执行删除操作的 [GH_TOKEN](https://github.com/ophub/amlogic-s9xxx-armbian/tree/main/build-armbian/documents#2-set-the-privacy-variable-github_token) 口令。 |
+| parameter/参数           | Defaults/默认值    | Description/说明                                                  |
+|-------------------------|-------------------|-------------------------------------------------------------------|
+| repo                    | `<owner>/<repo>`  | [Optional] Set the running repository, the default is the current repository. <br />[可选项] 设置运行仓库，默认为当前仓库。 |
+| delete_tags             | true              | [Optional] Delete tags associated with releases (options: true/false). <br />[可选项] 删除与 Releases 关联的 tags (选项：true/false)。 |
+| releases_keep_latest    | None / 无         | [Required] Set how many latest Releases versions to keep (Integer. eg: 5), Set to `0` to delete all. <br />[必选] 设置保留多少个最新的 Releases 版本（整数。如：5），设置为 `0` 表示全部删除。 |
+| releases_keep_keyword   | None / 无         | [Optional] The keywords of the releases that need to be reserved (string). <br />[可选项] 需要保留的 Releases 的关键字（字符串）。 |
+| workflows_keep_keyword  | None / 无         | [Optional] The keywords of workflows runs that need to be reserved (string). <br />[可选项] 需要保留的 workflows 运行记录的关键字（字符串）。 |
+| workflows_keep_day      | 30                | [Optional] Set the number of days to keep workflow runs (Integer. eg: 30), Set to `0` to delete all. <br />[可选项] 设置保留多少天以内的 workflows 记录（整数。如：30），设置为 `0` 表示全部删除。 |
+| out_log                 | false             | [Optional] Whether to output a listing log for each step (options: true/false). <br />[可选项] 是否为每个步骤输出一个列表日志 (选项：true/false)。 |
+| gh_token                | None / 无         | [Required] Set the [GH_TOKEN](https://github.com/ophub/amlogic-s9xxx-armbian/tree/main/build-armbian/documents#2-set-the-privacy-variable-github_token) for performing delete operations. <br />[必选] 设置执行删除操作的 [GH_TOKEN](https://github.com/ophub/amlogic-s9xxx-armbian/tree/main/build-armbian/documents#2-set-the-privacy-variable-github_token) 口令。 |
 
 ## Links / 链接
 
--- del.sh
+++ del.sh
@@ -39,7 +39,7 @@ init_var() {
     sudo apt-get -qq update && sudo apt-get -qq install -y jq
 
     # If it is followed by [ : ], it means that the option requires a parameter value
-    get_all_ver="$(getopt "r:t:l:w:k:d:g:" "${@}")"
+    get_all_ver="$(getopt "r:t:l:w:k:d:o:g:" "${@}")"
 
     while [[ -n "${1}" ]]; do
         case "${1}" in
@@ -75,7 +75,6 @@ init_var() {
                 error_msg "Invalid -w parameter [ ${2} ]!"
             fi
             ;;
-
         -k | --workflows_keep_keyword)
             if [[ -n "${2}" ]]; then
                 workflows_keep_keyword="${2}"
@@ -90,6 +89,14 @@ init_var() {
                 shift
             else
                 error_msg "Invalid -d parameter [ ${2} ]!"
+            fi
+            ;;
+        -o | --out_log)
+            if [[ -n "${2}" ]]; then
+                out_log="${2}"
+                shift
+            else
+                error_msg "Invalid -o parameter [ ${2} ]!"
             fi
             ;;
         -g | --gh_token)
@@ -113,13 +120,14 @@ init_var() {
     echo -e "${INFO} releases_keep_keyword: [ ${releases_keep_keyword} ]"
     echo -e "${INFO} workflows_keep_keyword: [ ${workflows_keep_keyword} ]"
     echo -e "${INFO} workflows_keep_day: [ ${workflows_keep_day} ]"
+    echo -e "${INFO} out_log: [ ${out_log} ]"
     echo -e ""
 }
 
 get_releases_list() {
     echo -e "${STEPS} Start generating the releases list..."
 
-    # Temporary save file for the results returned by the github API for releases
+    # Temporary save file for the results returned by api.github.com for releases
     gh_api_releases="josn_api_releases"
     curl -s \
         -H "Accept: application/vnd.github+json" \
@@ -127,13 +135,14 @@ get_releases_list() {
         https://api.github.com/repos/${repo}/releases \
         >${gh_api_releases}
     [[ -s "${gh_api_releases}" ]] || error_msg "(1.1) The api.github.com for releases query failed."
-    echo -e "${INFO} (1.1) github API for releases request succeeded."
+    echo -e "${INFO} (1.1) The api.github.com for releases request successfully."
 
     # All lists in releases
     all_releases_list="josn_all_releases_list"
     cat ${gh_api_releases} | jq -c '.[] | {pub_date: .published_at, id: .id, name: .name, tag_name: .tag_name}' >${all_releases_list}
     if [[ -s "${all_releases_list}" ]]; then
-        echo -e "${INFO} (1.2) All releases lists are generated successfully. Current list:\n$(cat ${all_releases_list})"
+        echo -e "${INFO} (1.2) The complete releases list is generated successfully."
+        [[ "${out_log}" == "true" ]] && echo -e "${INFO} (1.2) Current release list:\n$(cat ${all_releases_list})"
     else
         echo -e "${TIPS} (1.2) The releases list is empty."
     fi
@@ -144,9 +153,10 @@ get_releases_list() {
     if [[ -n "${releases_keep_keyword}" && -s "${all_releases_list}" ]]; then
         cat ${all_releases_list} | jq -r .name | grep ${releases_keep_keyword} >${keep_releases_keyword_list}
         [[ -s "${keep_releases_keyword_list}" ]] && cat ${keep_releases_keyword_list} | while read line; do sed -i "/${line}/d" ${all_releases_list}; done
-        echo -e "${INFO} (1.3) Keep keyword list filtered successfully. Current list:\n$(cat ${all_releases_list})"
+        echo -e "${INFO} (1.3) The keyword filtering successfully."
+        [[ "${out_log}" == "true" ]] && echo -e "${INFO} (1.3) Current releases list:\n$(cat ${all_releases_list})"
     else
-        echo -e "${TIPS} (1.3) Skip keep keyword releases filtering."
+        echo -e "${TIPS} (1.3) The filter keyword is empty. skip."
     fi
 
     # List of releases date to keep
@@ -155,19 +165,18 @@ get_releases_list() {
     keep_releases_list="josn_keep_releases_list"
     # Sort and generate a keep list of releases
     if [[ -s "${all_releases_list}" && -n "${releases_keep_latest}" ]]; then
+        # Sort by date and select the list of releases that need to be kept
         cat ${all_releases_list} | jq -r '.pub_date' | tr ' ' '\n' | sort -r | head -${releases_keep_latest} >${keep_releases_date_list}
-        cat ${keep_releases_date_list} | while read line; do grep "${line}" ${all_releases_list} >>${keep_releases_list}; done
-        echo -e "${INFO} (1.4) The keep releases list is generated successfully. Keep list:\n$(cat ${keep_releases_list})"
+        [[ -s "${keep_releases_date_list}" ]] && {
+            # Generate a complete json list for log output
+            cat ${keep_releases_date_list} | while read line; do grep "${line}" ${all_releases_list} >>${keep_releases_list}; done
+            # Remove releases that need to be kept from the full list
+            cat ${keep_releases_date_list} | while read line; do sed -i "/${line}/d" ${all_releases_list}; done
+        }
+        echo -e "${INFO} (1.4) The keep releases list is generated successfully."
+        [[ "${out_log}" == "true" && -s "${keep_releases_list}" ]] && echo -e "${INFO} (1.4) Keep releases list:\n$(cat ${keep_releases_list})"
     else
-        echo -e "${TIPS} (1.4) The releases list is empty. skip."
-    fi
-
-    # Remove the keep_releases_list from all_releases_list
-    if [[ -s "${keep_releases_date_list}" ]]; then
-        cat ${keep_releases_date_list} | while read line; do sed -i "/${line}/d" ${all_releases_list}; done
-        echo -e "${INFO} (1.5) Need to keep list filtered successfully."
-    else
-        echo -e "${TIPS} (1.5) The keep list is empty. skip."
+        echo -e "${TIPS} (1.4) The keep releases list is empty. skip."
     fi
 
     # The list to be deleted in releases
@@ -175,9 +184,10 @@ get_releases_list() {
     # Generate releases.id delete list
     if [[ -s "${all_releases_list}" ]]; then
         cat ${all_releases_list} >${del_releases_list}
-        echo -e "${SUCCESS} (1.6) Delete releases list generated successfully. Delete list:\n$(cat ${del_releases_list})"
+        echo -e "${SUCCESS} (1.5) The delete releases list generated successfully."
+        [[ "${out_log}" == "true" ]] && echo -e "${INFO} (1.5) Delete releases list:\n$(cat ${del_releases_list})"
     else
-        echo -e "${TIPS} (1.6) The delete releases list is empty. skip."
+        echo -e "${TIPS} (1.5) The delete releases list is empty. skip."
     fi
 
     echo -e ""
@@ -224,7 +234,7 @@ del_release_tag() {
 get_workflows_list() {
     echo -e "${STEPS} Start generating the workflows list..."
 
-    # Temporary save file for the results returned by the github API for workflows runs
+    # Temporary save file for the results returned by api.github.com for workflows runs
     gh_api_workflows="josn_api_workflows"
     curl -s \
         -H "Accept: application/vnd.github+json" \
@@ -232,13 +242,14 @@ get_workflows_list() {
         https://api.github.com/repos/${repo}/actions/runs \
         >${gh_api_workflows}
     [[ -s "${gh_api_workflows}" ]] || error_msg "(3.1) The api.github.com for workflows query failed."
-    echo -e "${INFO} (3.1) github API for workflows request succeeded."
+    echo -e "${INFO} (3.1) The api.github.com for workflows request successfully."
 
     # All lists in workflows
     all_workflows_list="josn_all_workflows_list"
-    cat ${gh_api_workflows} | jq -c '.workflow_runs[] | {pub_date: .updated_at, id: .id, name: .name}' >${all_workflows_list}
+    cat ${gh_api_workflows} | jq -c '.workflow_runs[] | select(.status != "in_progress") | {pub_date: .updated_at, id: .id, name: .name}' >${all_workflows_list}
     if [[ -s "${all_workflows_list}" ]]; then
-        echo -e "${INFO} (3.2) All workflows lists are generated successfully. Current list:\n$(cat ${all_workflows_list})"
+        echo -e "${INFO} (3.2) The complete workflows list is generated successfully."
+        [[ "${out_log}" == "true" ]] && echo -e "${INFO} (3.2) Current workflows list:\n$(cat ${all_workflows_list})"
     else
         echo -e "${TIPS} (3.2) The workflows list is empty."
     fi
@@ -249,9 +260,10 @@ get_workflows_list() {
     if [[ -n "${workflows_keep_keyword}" && -s "${all_workflows_list}" ]]; then
         cat ${all_workflows_list} | jq -r .name | grep ${workflows_keep_keyword} >${keep_keyword_workflows_list}
         [[ -s "${keep_keyword_workflows_list}" ]] && cat ${keep_keyword_workflows_list} | while read line; do sed -i "/${line}/d" ${all_workflows_list}; done
-        echo -e "${INFO} (3.3) Keep keyword list filtered successfully. Current list:\n$(cat ${all_workflows_list})"
+        echo -e "${INFO} (3.3) The keyword filtering successfully."
+        [[ "${out_log}" == "true" ]] && echo -e "${INFO} (3.3) Current workflows list:\n$(cat ${all_workflows_list})"
     else
-        echo -e "${TIPS} (3.3) Skip keep keyword workflows filtering."
+        echo -e "${TIPS} (3.3) The filter keyword is empty. skip."
     fi
 
     # Generate a date list of workflows
@@ -267,14 +279,17 @@ get_workflows_list() {
         cat ${all_workflows_date_list} | while read line; do
             line_second="$(date -d "${line//-/}" +%s)"
             day_diff="$(((${today_second} - ${line_second}) / 86400))"
-            [[ "${day_diff}" -le "${workflows_keep_day}" ]] && {
+            [[ "${day_diff}" -lt "${workflows_keep_day}" ]] && {
                 grep "${line}T" ${all_workflows_list} >>${keep_workflows_list}
                 sed -i "/${line}T/d" ${all_workflows_list}
             }
         done
+        echo -e "${INFO} (3.4) The keep workflows list is generated successfully."
         # Remove duplicate lines
-        awk '!a[$0]++' ${keep_workflows_list} >${tmp_josn_file} && mv -f ${tmp_josn_file} ${keep_workflows_list}
-        echo -e "${INFO} (3.4) The keep workflows list is generated successfully. Keep list:\n$(cat ${keep_workflows_list})"
+        [[ -s "${keep_workflows_list}" ]] && {
+            awk '!a[$0]++' ${keep_workflows_list} >${tmp_josn_file} && mv -f ${tmp_josn_file} ${keep_workflows_list}
+            [[ "${out_log}" == "true" ]] && echo -e "${INFO} (3.4) Keep workflows list:\n$(cat ${keep_workflows_list})"
+        }
     else
         echo -e "${TIPS} (3.4) The workflows list is empty. skip."
     fi
@@ -284,7 +299,8 @@ get_workflows_list() {
     # Generate workflows.id delete list
     if [[ -s "${all_workflows_list}" ]]; then
         cat ${all_workflows_list} >${del_workflows_list}
-        echo -e "${SUCCESS} (3.5) Delete workflows list generated successfully. Delete list:\n$(cat ${del_workflows_list})"
+        echo -e "${SUCCESS} (3.5) The delete workflows list generated successfully."
+        [[ "${out_log}" == "true" ]] && echo -e "${INFO} (3.5) Delete workflows list:\n$(cat ${del_workflows_list})"
     else
         echo -e "${TIPS} (3.5) The delete workflows list is empty. skip."
     fi
@@ -297,13 +313,13 @@ del_workflows_runs() {
 
     # Delete workflows runs
     if [[ -s "${del_workflows_list}" && -n "$(cat ${del_workflows_list} | jq -r .id)" ]]; then
-        cat ${del_workflows_list} | jq -r .id | while read workflows_id; do
+        cat ${del_workflows_list} | jq -r .id | while read run_id; do
             {
                 curl -s \
                     -X DELETE \
                     -H "Accept: application/vnd.github+json" \
                     -H "Authorization: Bearer ${gh_token}" \
-                    https://api.github.com/repos/${repo}/actions/runs/${workflows_id}
+                    https://api.github.com/repos/${repo}/actions/runs/${run_id}
             }
         done
         echo -e "${SUCCESS} (4.1) Workflows runs deleted successfully."
